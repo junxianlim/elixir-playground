7f662d562c4e3969d3fbc690f5ad9464985004ec
Lim Jun Xian <eldrethlim@gmail.com>
2017-10-11 00:07:12 +0800

    adds merge sort

--- merge_sort/merge_sort.ex
+++ merge_sort/merge_sort.ex
@@ -1,5 +1,32 @@
 defmodule MergeSort do
-  def run(list) when length(list)<2, do: list
-  def run(list) when is_list(list) do
+  def run([]), do: []
+  def run([x]), do: [x] 
+  def run(list) do
+    { l, r } = Enum.split(list, div(length(list), 2))
+    merge(run(l), run(r), [])
+  end
+
+  def merge(h, [], acc), do: acc ++ h
+  def merge([], t, acc), do: acc ++ t
+  def merge([lh | lt] = l, [rh | rt] = r, acc) do
+    cond do
+      lh > rh -> merge(l, rt, acc ++ [rh])
+      true    -> merge(lt, r, acc ++ [lh])
+    end
+  end
+end
+
+# Test cases
+ExUnit.start()
+
+defmodule MergeSortTest do
+  use ExUnit.Case
+
+  test "does selection sort correctly" do
+    assert MergeSort.run([2])                 == [2]
+    assert MergeSort.run([2,3,1])             == [1,2,3]
+    assert MergeSort.run([4,3,4,2,1])         == [1,2,3,4,4]
+    assert MergeSort.run([8,10,11,1,6,1,5,2]) == [1,1,2,5,6,8,10,11]
+    assert MergeSort.run([3,2,1,4,5,2])       == [1, 2, 2, 3, 4, 5]
   end
 end
