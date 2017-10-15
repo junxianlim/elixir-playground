4187002b407d04b59117c6ffb8c4a60944396176
Lim Jun Xian <eldrethlim@gmail.com>
2017-10-16 00:13:41 +0800

    fixes karatsuba algorithm

--- karatsuba/karatsuba.ex
+++ karatsuba/karatsuba.ex
@@ -1,5 +1,5 @@
 defmodule Karatsuba do
-  def run(x,y) when x < 10 or y < 10  do
+  def run(x,y) when x < 10 or y < 10 do
     x * y
   end
 
@@ -7,28 +7,28 @@ defmodule Karatsuba do
     x_length = get_length(x)
     y_length = get_length(y)
 
-    m = Enum.max([x_length, y_length]) / 2 |> round
+    m = Enum.max([x_length, y_length]) |> div(2)
 
-    { a, b } = divmod(x, ten_pow(m))
-    { c, d } = divmod(y, ten_pow(m))
+    { a, b } = divmod(x, pow(10, m))
+    { c, d } = divmod(y, pow(10, m))
 
-    ac = run(a, c)
-    bd = run(b, d)
+    ac = run(a,c)
+    bd = run(b,d)
 
     ad_plus_bc = run(a + b, c + d) - ac - bd
 
-    ac * ten_pow(m * 2) + (ad_plus_bc * ten_pow(m)) + bd
+    ac * pow(10, m * 2) + (ad_plus_bc * pow(10, m)) + bd
   end
 
-  defp ten_pow(pow), do: :math.pow(10, pow) |> round
-  
-  defp get_length(num), do: num |> Integer.digits |> Enum.count
-
-  defp divmod(num, m) do
-    rem = Integer.mod(num, m)
-    div = (num - rem) / m |> round
-    { div, rem }
+  def divmod(num, m) do
+    r = rem(num, m)
+    { div(num - r, m), r }
   end
+
+  def get_length(num), do: num |> round |> Integer.digits |> Enum.count
+  def pow(n, e), do: pow(n, e, 1)
+  def pow(_, 0, acc), do: acc
+  def pow(n, e, acc), do: pow(n, e - 1, n * acc)
 end
 
 # Test cases
@@ -44,5 +44,6 @@ defmodule KaratsubaTest do
     assert Karatsuba.run(1234, 6789)        == 1234 * 6789
     assert Karatsuba.run(5555555, 12345)    == 5555555 * 12345
     assert Karatsuba.run(75081230, 1203123) == 75081230 * 1203123
+    assert Karatsuba.run(3141592653589793238462643383279502884197169399375105820974944592,2718281828459045235360287471352662497757247093699959574966967627) == 8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184
   end
 end
