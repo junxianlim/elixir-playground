640efd1873ab08f31c5b8ac1c5b0628c19a6832f
Lim Jun Xian <eldrethlim@gmail.com>
2017-10-22 16:58:03 +0800

    adds test cases

--- fibonnaci/fibonnaci.ex
+++ fibonnaci/fibonnaci.ex
@@ -2,12 +2,8 @@ defmodule Fibonnaci do
   def run(0), do: 0
   def run(1), do: 1
   def run(ctr), do: add(0, 1, ctr - 1)
-
   def add(_x, y, 0), do: y
-  def add(x, y, ctr) do
-    x = x + y
-    add(y, x, ctr - 1)
-  end
+  def add(x, y, ctr), do: add(y, x + y, ctr - 1)
 end
 
 # Test cases
@@ -27,5 +23,6 @@ defmodule FibonnaciTest do
     assert Fibonnaci.run(50)  == 12586269025
     assert Fibonnaci.run(100) == 354224848179261915075
     assert Fibonnaci.run(200) == 280571172992510140037611932413038677189525
+    assert Fibonnaci.run(500) == 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125
   end
 end
