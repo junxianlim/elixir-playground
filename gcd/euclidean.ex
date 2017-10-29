9dbf0d2eea088135cb95518e1286d72ae0545546
Lim Jun Xian <eldrethlim@gmail.com>
2017-10-29 14:24:58 +0800

    adds gcd

---
+++ gcd/euclidean.ex
@@ -0,0 +1,21 @@
+defmodule Euclidean do
+  def run(0, y), do: y
+  def run(x, 0), do: x
+  def run(x, y), do: run(y, rem(x,y))
+end
+
+# Test cases
+ExUnit.start()
+
+defmodule EuclideanTest do
+  use ExUnit.Case
+
+  test "does Fibonnaci correctly" do
+    assert Euclidean.run(0,1)             == 1
+    assert Euclidean.run(100,0)           == 100
+    assert Euclidean.run(10,3)            == 1
+    assert Euclidean.run(10,4)            == 2
+    assert Euclidean.run(234324,150123)   == 3
+    assert Euclidean.run(3918848,1653264) == 61232
+  end
+end
